93e72108236d17ae061cc30bd6a4822da22e6c5f
mSenyor <moran.senyor@gmail.com>
2015-10-05 00:08:05 +0300

    initial layout tests with a simple image to represent areas

---
+++ main.py
@@ -0,0 +1,27 @@
+__version__ = '0.1'
+
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.widget import Widget
+##from kivy.uix.
+##from kivy.uix.
+##from kivy.uix.
+
+class DipSwitchPanel(Widget):
+    def update_address():
+        pass
+
+class AddressPanel(Widget):
+    def update_dipswitch():
+        pass
+
+class AppWindow(Widget):
+    pass
+
+class DipSwitch(App):
+    def build(self):
+        return AppWindow()
+
+if __name__ == "__main__":
+    DipSwitch().run()
