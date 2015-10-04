5e16cbf5a9a1f9322b3d7b33f488659f27b1c99a
mSenyor <moran.senyor@gmail.com>
2015-10-05 02:52:57 +0300

    initial functionality with a rough UI

--- main.py
+++ main.py
@@ -2,26 +2,34 @@ __version__ = '0.1'
 
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.widget import Widget
-##from kivy.uix.
-##from kivy.uix.
-##from kivy.uix.
 
-class DipSwitchPanel(Widget):
-    def update_address():
-        pass
+class MainView(BoxLayout):
+    def input_to_binary(self):
 
-class AddressPanel(Widget):
-    def update_dipswitch():
-        pass
+        in_txt = self.ids['address']
 
-class AppWindow(Widget):
-    pass
+        label = self.ids['binary']
+        try:
+            int_add=int(in_txt.text)
+        except ValueError:
+            int_add=0
+            print "value error"
+
+        print "int_add: %d" % int_add
+        #if int_add != 0 and isinstance(in_txt.text, int):
+        if int_add != 0 and int(in_txt.text) == float(in_txt.text):
+            print "int(in_txt.text) == float(in_txt.text)"
+
+            if int(in_txt.text) < 512:
+                label.text='{0:09b}'.format(int(in_txt.text))
+            else:
+                label.text='Bad Value'
+        else:
+            label.text='Bad Value'
 
 class DipSwitch(App):
     def build(self):
-        return AppWindow()
+        return MainView()
 
 if __name__ == "__main__":
     DipSwitch().run()
